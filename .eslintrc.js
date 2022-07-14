da3dcb7cf75f8f72c8329b01f325979475f0ae62
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2022-07-14 21:45:29 +1000

    Installed nodemon, config eslint, tsconfig, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,60 @@
 module.exports = {
   env: {
-    es2021: true,
-    node: true,
+    es6: true,
+    node: true
   },
-  extends: [
-    'airbnb-base',
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    project: './tsconfig.eslint.json'
   },
-  plugins: [
-    '@typescript-eslint',
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:import/errors',
+    'plugin:import/typescript',
   ],
   rules: {
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/explicit-function-return-type': ['error', { 'allowExpressions': true }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'variableLike',
+        'format': ['PascalCase', 'camelCase', 'UPPER_CASE'],
+        'leadingUnderscore': 'allow',
+      },
+    ],
+    'no-unused-vars': 'off', // Disable base rule first
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+    'prettier/prettier': ['error',
+      {
+        'endOfLine':'auto'
+      }
+    ],
+    'require-atomic-updates': 'error',
+    'jest/no-mocks-import': 'off',
+    'jest/no-conditional-expect': 'warn',
+    'import/no-extraneous-dependencies': ['error'],
+    'no-restricted-imports': ['error', {
+      'patterns': ['ormconfig']
+    }],
   },
-};
+  overrides: [
+    {
+      files: ['src/migrations/*','src/seeds/*'],
+      rules: {
+        '@typescript-eslint/no-empty-function': 'off'
+      }
+    }
+  ]
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv-safe'
+import { add } from './math/add'
+
+dotenv.config()
+
+console.log(add(1, 4))
+console.log(process.env.MY_NAME)
