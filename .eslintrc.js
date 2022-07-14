69142c23e0778202d692458aa56db3a1023caa2f
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2022-07-14 22:04:28 +1000

    Finished boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
+    jest: true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
---
+++ src/math/add.test.ts
@@ -0,0 +1,7 @@
+import { add } from './add'
+
+describe('test should work', () => {
+  it('well', () => {
+    expect(add(1, 2)).toEqual(3)
+  })
+})
